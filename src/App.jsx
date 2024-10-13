dc296a13fcd486de5b4757e093ce33a126c14863
stevenduval <stevenduval@gmail.com>
2024-10-12 23:35:20 -0400

    working on components and fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
-import { useState } from 'react';
+
+import { Navigate, Route, Routes, useLocation, useParams } from 'react-router-dom';
 
 import './index.css'
 import apiKey from './config';
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
 import PhotoList from './components/PhotoList';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 const App = () => {
- 
+  // get current location
+  const location = useLocation();
+  // set states
+  const [term, setTerm] = useState(location.pathname.replace('/', ''));
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+  // fetch data from flicker API
+  const fetchData = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}s&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => setData(response.data.photos.photo))
+      .catch(error => console.log('Error:', error));
+  };
+  // 
+  useEffect(()=> {
+    // lets us know if we are still actively fetching the current term
+    let activeFetch = true;
+    // if location doesn't include search/ setTerm value equal to it
+    if (!location.pathname.includes('search/') && term !== location.pathname.replace('/', '') ) {
+      setTerm(location.pathname.replace('/', ''))
+    }
+    // if we are still using the current fetch
+    // need to fix as i am still running into race condition issues cause the data setting happens in the fetch
+    if (activeFetch) { 
+      console.log(term);
+      fetchData(term);
+      console.log('ww')
+      setLoading(false); 
+    }
+    // clean up function to stop fetch if it is called again
+    return () => { activeFetch = false; console.log('stopping old') }
+  }, [location, term]);
+
   return (
-    <div class="container">
+    <div className="container">
       <SearchForm />
       <Nav />
-      <PhotoList />
+      <Routes>
+        <Route path='/' end>
+          <Route index element={<Navigate replace to="cats"/>}/>
+        </Route>
+        <Route path=':term' element={<PhotoList term={term} data={data}/>}/>
+        <Route path='search/:term' element={<PhotoList term={term} data={data}/>}/>
+      </Routes>
     </div>
   )
 }
--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -1,15 +1,18 @@
+import { useParams } from "react-router-dom";
+
 import NotFound from "./NotFound";
 import Photo from "./Photo";
 
-const PhotoList = () => 
-
-    <div class="photo-container">
-        <h2>Results</h2>
-        <ul>
-            <Photo />
-            <NotFound />
-        </ul>
-    </div>
-;
+const PhotoList = ( { term, data } ) => {
+    return (
+        <div className="photo-container">
+            <h2>{term} Gifs</h2>
+            <ul>
+                <Photo />
+                <NotFound />
+            </ul>
+        </div>
+    )
+};
 
 export default PhotoList;
